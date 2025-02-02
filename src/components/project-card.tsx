e53d8c779783f37fdd4524dccc27cc14c34b137d
simon huang <simon.huang@outlook.de>
2025-02-02 14:11:20 +0100

    remove tags

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -4,9 +4,7 @@ import {
   CardContent,
   CardDescription,
   CardTitle,
-  CardFooter,
 } from "./ui/card";
-import { Badge } from "./ui/badge";
 
 interface Props {
   title: string;
@@ -15,9 +13,9 @@ interface Props {
   tags?: readonly string[];
 }
 
-export function ProjectCard({ title, description, tags, link }: Props) {
+export function ProjectCard({ title, description, link }: Props) {
   return (
-    <Card className="border-muted flex flex-col justify-between gap-6 overflow-hidden rounded-xl border px-4 py-3 pb-0 shadow-sm">
+    <Card className="border-muted flex flex-col justify-between gap-6 overflow-hidden rounded-xl border px-4 py-3 shadow-sm">
       <div>
         <CardHeader className="">
           <div className="space-y-1">
@@ -43,16 +41,6 @@ export function ProjectCard({ title, description, tags, link }: Props) {
           <CardDescription className=" text-xs">{description}</CardDescription>
         </CardContent>
       </div>
-
-      <CardFooter className="overflow-x-auto">
-        <div className="mb-3 flex min-w-0 flex-nowrap gap-2 p-0.5">
-          {tags?.map((tag) => (
-            <Badge variant="secondary" key={tag} className="shrink-0">
-              {tag}
-            </Badge>
-          ))}
-        </div>
-      </CardFooter>
     </Card>
   );
 }
